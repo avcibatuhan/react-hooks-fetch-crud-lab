a16b8ff2aa547a4e69881740cedd5426a025e3c5
batuhan <avcibatuhan26@gmail.com>
2022-02-07 11:56:44 +0300

    fetch crud lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,23 +1,27 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question,deleteQuestion,editQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
-
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
   ));
-
+  const onHandleDeleteQuestion = () => {
+    deleteQuestion(id)
+  }
+  const onHandleChangeSelect = (e) => {
+    editQuestion(id,e.target.value)
+  }
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={onHandleChangeSelect} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={onHandleDeleteQuestion}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions,setQuestions] = useState([]);
+  const [isLoaded,setIsloaded] = useState(false)
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then((response)=>response.json())
+    .then((data) => {
+      setQuestions(data)
+      setIsloaded(true)
+    });
+  },[]);
+
+  function deleteQuestion(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        const questionsToDisplay = questions.filter((q) => q.id !== id);
+        setQuestions(questionsToDisplay);
+      });
+  }
+  function handleEditQuestion(id,correctIndex){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then((r) => r.json())
+      .then((editedQuestion) => {
+        console.log(editedQuestion)
+        const questionsToDisplay = questions.map((question) => {
+          if (question.id === editedQuestion.id) return editedQuestion;
+          return question;
+        });
+        setQuestions(questionsToDisplay);
+      });
+  }
+  const questionItems = questions.map((question) => (
+    <QuestionItem
+      key={question.id}
+      question={question} 
+      deleteQuestion={deleteQuestion}
+      editQuestion={handleEditQuestion}
+    />
+  ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{isLoaded === true ? questionItems : "Yükleniyor"}</ul>
     </section>
   );
 }
 
-export default QuestionList;
+
+//  export function QuestionItemRender({ questions }) {
+   
+//    const array = questions[0]
+//    console.log(array)
+//    return(<div>
+//      {
+//        array.map((question)=> {
+//          <h1>{question.id}</h1>
+//        })
+//      }
+//    </div>)
+//    return (
+//      questions.map((question) => {
+//        <QuestionItem question={question} />
+
+//            })
+//    );
+//  }
+ export default QuestionList;
